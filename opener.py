0fcc29c374f6906ff916b1ba8d49bf744915e6c5
Thomas <terickson@spotxchange.com>
2016-02-11 17:58:52 -0700

    this file will do the actual opening and converting csv to a list

--- opener.py
+++ opener.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python
 import webbrowser
 import csv
+import numpy as np
 
-with open('link1.csv') as f:
-	lis = [x.split() for x in f]
-for x in zip(*lis)
-	for y in x:
-		print(y+'\t', end='')
-		print('n')
-'''new_file = []
 b = webbrowser.get('firefox')
-'''new_row = []
-for row in file1:
-	new_row.append(row)
-	new_file.append(tuple(new_row))'''
-for row in file1:
-	new_file.append(row)
-	zip(*new_file)
-	print(new_file)'''
+with open ('link.csv', 'rb') as f:
+	reader = csv.reader(f)
+	f1 = list(reader)
+	print type(f1)
+	for x in f1:
+		print(x)
+		b.open(x)
