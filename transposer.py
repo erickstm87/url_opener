b905949a5062ab2ddb6ce5b1562c36df37d067f1
erickstm87 <erickstm@colorado.edu>
2016-02-11 21:10:24 -0700

    opening files and transposing columns

--- transposer.py
+++ transposer.py
@@ -2,10 +2,15 @@
 import webbrowser
 import csv
 import numpy as np
+import sys
 
+b = webbrowser.get('firefox')
 new_file = []
 file1 = open('text.txt')
 for row in file1:
 	new_file.append(row)
 np.transpose(new_file)
-print(new_file)
+for i in new_file:
+    b.open(i)
+#sys.stdout = open('link.txt','w')
+#print(new_file)
