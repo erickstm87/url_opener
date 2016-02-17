6e33bb660a83e4fdfdc3e27aa29c3667e844bb26
Thomas <terickson@spotxchange.com>
2016-02-17 14:40:59 -0700

    this is the mouse tracker to tell you your position on the screen

---
+++ mouse_posit.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.position()
+print ('press ctrl-c to end program')
+try:
+        while True:
+                print (pyautogui.position())
+except KeyboardInterrupt:
+        print('\ndone')
