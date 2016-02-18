480b7dd5f9a8cc4412f0151d7537f7ac5d1ca3a5
Thomas <terickson@spotxchange.com>
2016-02-18 11:32:52 -0700

    this works fully with integration urls

--- mac_version.py
+++ mac_version.py
@@ -16,18 +16,29 @@ b = webbrowser.get('safari')
 bashCommand = "sudo killall 'Safari' "
 #bashCommand = "sudo killall -r 'google'"
 new = 1
-file1 = open('tester.txt', 'r')
+file1 = open('int_test.txt', 'r')
 x = 0
 for i in file1:
     x += 1
     b.open(i,new=new)
     #webbrowser.open(i,new=new)
     time.sleep(3)
-    pyautogui.click(1015,105)
+    pyautogui.click(548,438)
     time.sleep(2)
-    call(["screencapture", "screenshot(%s).jpg" % x]) 
-    pyautogui.click(879,197)
-    call(["screencapture", "screenshot_close(%s).jpg" % x])
+    call(["screencapture", "screenshot_play(%s).jpg" % x]) 
+    time.sleep(2)
+    pyautogui.click(809,808)
+    time.sleep(4)
+    call(["screencapture", "screenshot_full(%s).jpg" % x])
+    pyautogui.click(1430,890)
+    time.sleep(2)
+    call(["screencapture", "screenshot_full_exit(%s).jpg" % x])
+    pyautogui.click(270,807)
+    call(["screencapture", "screenshot_pause(%s).jpg" % x])
+    time.sleep(3)
+    pyautogui.click(270,807)
+    call(["screencapture", "screenshot_replay(%s).jpg" % x])
+    time.sleep(10)
     os.system(bashCommand)
     '''if x==1:
         pyautogui.click(530,44)
