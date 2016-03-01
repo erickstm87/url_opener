7a1c6f40aa434b887ec6f47223b82c227b1940ea
Thomas <terickson@spotxchange.com>
2016-03-01 14:05:11 -0700

    need to get locate functions working

--- safari_easi.py
+++ safari_easi.py
@@ -13,49 +13,80 @@ pyautogui.size()
 width,height = pyautogui.size()
 b = webbrowser.get('safari')
 bashCommand = "sudo killall 'Safari' "
-#bashCommand = "sudo killall -r 'google'"
 new = 1
-file1 = open('another.txt', 'r')
+file1 = open('incon.txt', 'r')
 #file1 = raw_input('whats the filename of your urls? make sure its a txt file, you dont need to type in the file extension \n') + '.csv'
 x = 0
 
-def incontent_noauto(i):
-    j = 0
-    b.open(i,new=new)
+def ad_spotter():
+    x,y = pyautogui.locateCenterOnScreen('ad_here.png')
+    pyautogui.moveTo(x,(y-10))
+
+def full_screen():
+    time.sleep(2)
+    x, y = pyautogui.locateCenterOnScreen('full_screen.png')
+    pyautogui.click(x,y)
     time.sleep(3)
+
+def scroll_down():
+    j = 0
     for j in range(0,37):
         pyautogui.press('down')
     pyautogui.press('s')#starts the ad
     time.sleep(2)
+
+def scroll_up():
+    j = 0
+    for j in range(0,38):
+        pyautogui.press('up')
+
+def close_tab():
+    pyautogui.keyDown('command')
+    pyautogui.press('w')
+    pyautogui.keyUp('command')
+
+def incontent_noauto(i):
+    b.open(i,new=new)
+    time.sleep(3)
+    scroll_down()
+    ad_spotter()
     #pyautogui.click('')#write this out to clickthrough
     #pyautogui.keydown('command')#closes out the tab
     #pyautogui.keypress('w')
     #pyautogui.keyup('command')
-    time.sleep(2)
-    #pyautogui.moveTo(630,668)#this is to go full screen
-    time.sleep(1)
-    #pyautogui.press('esc')
+    full_screen()
+    pyautogui.press('esc')
     time.sleep(2)
     pyautogui.press('p')#pause the ad
     time.sleep(1)
     pyautogui.press('r')#resume the ad
-    for j in range(0,38):
-        pyautogui.press('up')
-    os.system(bashCommand)
-'''
-def incontent_auto():
-    j = 0 
+    time.sleep(15)
+    scroll_up()
+    #os.system(bashCommand)
+    close_tab()
+
+def incontent_auto(i):
     b.open(i,new=new)
-    for j in range(0,16):
-        pyautogui.press('down')
-    #ad interactivity still to come
-'''
+    time.sleep(3)
+    scroll_down()
+    ad_spotter()
+    time.sleep(5)
+    pyautogui.press('p')
+    time.sleep(2)
+    pyautogui.press('r')
+    time.sleep(15)
+    full_screen()
+    pyautogui.press('esc')
+    scroll_up()
+    #os.system(bashCommand)
+    close_tab()
+
 def instream_noauto(i):
     b.open(i,new=new)
     time.sleep(3)
     pyautogui.press('s')#starts the ad
     time.sleep(2)
-    pyautogui.click('')#write this out to clickthrough
+    #pyautogui.click('')#write this out to clickthrough
     '''pyautogui.keydown('command')#closes out the tab
     pyautogui.keypress('w')
     pyautogui.keyup('command')'''
@@ -67,7 +98,11 @@ def instream_noauto(i):
     pyautogui.press('p')#pause the ad
     time.sleep(1)
     pyautogui.press('r')#resume the ad
-    os.system(bashCommand)
+    time.sleep(15)
+    #os.system(bashCommand)
+    pyautogui.keyDown('command')
+    pyautogui.press('w')
+    pyautogui.keyUp('command')
 
 def instream_auto(i):
     b.open(i,new=new)
@@ -85,19 +120,22 @@ def instream_auto(i):
     time.sleep(1)
     pyautogui.press('r')#resume the ad
     time.sleep(10)
-    os.system(bashCommand)
+    #os.system(bashCommand)
+    pyautogui.keyDown('command')
+    pyautogui.press('w')
+    pyautogui.keyUp('command')
 
 def main():
-    
+
     for i in file1:
-        try:
-            if 'incontent' and 'spotx_autoplay=0' in i:
-                incontent_noauto(i)
-            elif 'instream' and 'spotx_autoplay=0' in i:
-                instream_noauto(i)
-            elif 'instrem' and 'spotx_autoplay=&' in i:
-                instream_auto(i)
-        except KeyboardInterrupt: 
+        #try:
+        if 'incontent' and 'spotx_autoplay=0' in i:
+            incontent_noauto(i)
+        elif 'instream' and 'spotx_autoplay=0' in i:
+            instream_noauto(i)
+        elif 'instrem' and 'spotx_autoplay=&' in i:
+            instream_auto(i)
+        '''except KeyboardInterrupt: 
             print ("\nPausing... (Hit ENTER to continue, type quit to exit, or c to copy the url to the failed file.)")
             try:
                 response = raw_input()
@@ -109,7 +147,7 @@ def main():
                         continue
             except KeyboardInterrupt:
                 print ('Resuming...')
-                continue
+                continue'''
 main()
     
 
